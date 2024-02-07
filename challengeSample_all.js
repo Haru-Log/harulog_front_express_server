fd8e00de3d52b6d8b0260344dee9fea9b5af3c92
childevnote <daniel011125@gmail.com>
2024-02-07 12:20:57 +0900

    Update challengeSample_all.js

--- challengeSample_all.js
+++ challengeSample_all.js
@@ -9,66 +9,66 @@ const dummyChallengeAll = [
   },
   {
     challengeId: 2,
-    challengeTitle: "체력과 근력을 키우는 30일 운동 챌린지",
+    challengeTitle: "근력을 키우는 30일 운동 챌린지",
     categoryName: "운동",
     challengeUserCount: 15,
-    imageUrl: "https://images.unsplash.com/photo-1513451713350-dee890297c73"
+    imageUrl: "https://images.unsplash.com/photo-1517836357463-d25dfeac3438?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fCVFQyU5QSVCNCVFQiU4RiU5OXxlbnwwfHwwfHx8MA%3D%3D"
   },
   {
     challengeId: 3,
-    challengeTitle: "새로운 날씨 정보를 알아가는 30일 기상 챌린지",
+    challengeTitle: "새로운 기상 챌린지",
     categoryName: "기상",
     challengeUserCount: 22,
-    imageUrl: "https://images.unsplash.com/photo-1523545441868-8e2fc91a36df"
+    imageUrl: "https://images.unsplash.com/photo-1552650272-b8a34e21bc4b?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     challengeId: 4,
-    challengeTitle: "매일 책 한 권 읽는 30일 독서 챌린지",
+    challengeTitle: "매일 책 한 권 독서 챌린지",
     categoryName: "독서",
     challengeUserCount: 18,
-    imageUrl: "https://images.unsplash.com/photo-1463107974121-d353d7c74645"
+    imageUrl: "https://images.unsplash.com/photo-1506880018603-83d5b814b5a6?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     challengeId: 5,
-    challengeTitle: "집중력 향상을 위한 공부 습관 형성 챌린지",
+    challengeTitle: "집중력 향상 공부 습관 챌린지",
     categoryName: "공부",
     challengeUserCount: 29,
-    imageUrl: "https://images.unsplash.com/photo-1497366754035-f200968a6b2f"
+    imageUrl: "https://images.unsplash.com/photo-1495465798138-718f86d1a4bc?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     challengeId: 6,
-    challengeTitle: "유연성과 근력을 키우는 30일 운동 챌린지",
+    challengeTitle: "유연성을 키우는 30일 운동 챌린지",
     categoryName: "운동",
     challengeUserCount: 12,
-    imageUrl: "https://images.unsplash.com/photo-1487730116645-74489c95b41b"
+    imageUrl: "https://images.unsplash.com/photo-1544367567-0f2fcb009e0b?q=80&w=1820&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     challengeId: 7,
-    challengeTitle: "날씨 변화에 따른 운동 스케줄 조정 챌린지",
+    challengeTitle: "날씨 변화에 따른 기상 챌린지",
     categoryName: "기상",
     challengeUserCount: 20,
-    imageUrl: "https://images.unsplash.com/photo-1496167117682-0a290907cdbd"
+    imageUrl: "https://images.unsplash.com/photo-1601134467661-3d775b999c8b?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     challengeId: 8,
-    challengeTitle: "매일 새로운 기상 패턴을 기록하는 30일 기상 챌린지",
+    challengeTitle: "매일 새로운 기상 패턴 챌린지",
     categoryName: "기상",
     challengeUserCount: 25,
-    imageUrl: "https://images.unsplash.com/photo-1529778873920-533cdb16a793"
+    imageUrl: "https://plus.unsplash.com/premium_photo-1661775884128-d971ffd2d300?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     challengeId: 9,
     challengeTitle: "하루에 한 시간 독서하기 챌린지",
     categoryName: "독서",
     challengeUserCount: 17,
-    imageUrl: "https://images.unsplash.com/photo-1489447068241-7db849f7b83f"
+    imageUrl: "https://images.unsplash.com/photo-1598024055266-e772a5f8c128?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     challengeId: 10,
-    challengeTitle: "공부 시간을 늘리는 30일 공부 챌린지",
+    challengeTitle: "공부 시간을 늘리는 공부 챌린지",
     categoryName: "공부",
     challengeUserCount: 28,
-    imageUrl: "https://images.unsplash.com/photo-1505682634904-6485d3d91d94"
+    imageUrl: "https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   }
 
 ];
