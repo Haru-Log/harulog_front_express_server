e0432bbd34cdcc06b526baf32197f19f63d7280c
childevnote <daniel011125@gmail.com>
2024-02-09 01:15:35 +0900

    Chore: dummy change

--- challengeSample_all.js
+++ challengeSample_all.js
@@ -2,75 +2,75 @@ const dummyChallengeAll = [
 
   {
     challengeId: 1,
-    challengeTitle: "뇌를 단련하는 30일 공부 챌린지",
-    categoryName: "공부",
-    challengeUserCount: 26,
-    imageUrl: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e"
-  },
-  {
-    challengeId: 2,
-    challengeTitle: "근력을 키우는 30일 운동 챌린지",
+    challengeTitle: "데일리 오운완",
     categoryName: "운동",
-    challengeUserCount: 15,
+    challengeUserCount: 26,
     imageUrl: "https://images.unsplash.com/photo-1517836357463-d25dfeac3438?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fCVFQyU5QSVCNCVFQiU4RiU5OXxlbnwwfHwwfHx8MA%3D%3D"
   },
   {
-    challengeId: 3,
-    challengeTitle: "새로운 기상 챌린지",
-    categoryName: "기상",
-    challengeUserCount: 22,
-    imageUrl: "https://images.unsplash.com/photo-1552650272-b8a34e21bc4b?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    challengeId: 2,
+    challengeTitle: "매일 30분 공부 인증",
+    categoryName: "공부",
+    challengeUserCount: 15,
+    imageUrl: "https://images.unsplash.com/photo-1432888498266-38ffec3eaf0a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8JUVBJUIzJUI1JUVCJUI2JTgwfGVufDB8fDB8fHww"
   },
   {
-    challengeId: 4,
-    challengeTitle: "매일 책 한 권 독서 챌린지",
+    challengeId: 3,
+    challengeTitle: "한 달에 한 권씩",
     categoryName: "독서",
-    challengeUserCount: 18,
+    challengeUserCount: 22,
     imageUrl: "https://images.unsplash.com/photo-1506880018603-83d5b814b5a6?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
+    challengeId: 4,
+    challengeTitle: "일찍 일어나는 새가",
+    categoryName: "기상",
+    challengeUserCount: 18,
+    imageUrl: "https://images.unsplash.com/photo-1552650272-b8a34e21bc4b?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  },
+  {
     challengeId: 5,
-    challengeTitle: "집중력 향상 공부 습관 챌린지",
-    categoryName: "공부",
+    challengeTitle: "구름 피트니스",
+    categoryName: "운동",
     challengeUserCount: 29,
-    imageUrl: "https://images.unsplash.com/photo-1495465798138-718f86d1a4bc?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    imageUrl: "https://plus.unsplash.com/premium_photo-1663011511327-68549a941247?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     challengeId: 6,
-    challengeTitle: "유연성을 키우는 30일 운동 챌린지",
-    categoryName: "운동",
+    challengeTitle: "다함께 스터디",
+    categoryName: "공부",
     challengeUserCount: 12,
-    imageUrl: "https://images.unsplash.com/photo-1544367567-0f2fcb009e0b?q=80&w=1820&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    imageUrl: "https://images.unsplash.com/photo-1531482615713-2afd69097998?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     challengeId: 7,
-    challengeTitle: "날씨 변화에 따른 기상 챌린지",
-    categoryName: "기상",
+    challengeTitle: "비소설 도서 읽기",
+    categoryName: "독서",
     challengeUserCount: 20,
-    imageUrl: "https://images.unsplash.com/photo-1601134467661-3d775b999c8b?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    imageUrl: "https://images.unsplash.com/photo-1491309055486-24ae511c15c7?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     challengeId: 8,
-    challengeTitle: "매일 새로운 기상 패턴 챌린지",
+    challengeTitle: "아침에 물 한잔",
     categoryName: "기상",
     challengeUserCount: 25,
-    imageUrl: "https://plus.unsplash.com/premium_photo-1661775884128-d971ffd2d300?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    imageUrl: "https://images.unsplash.com/photo-1549372691-289fcc650e4e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     challengeId: 9,
-    challengeTitle: "하루에 한 시간 독서하기 챌린지",
-    categoryName: "독서",
+    challengeTitle: "스트레칭의 중요성",
+    categoryName: "운동",
     challengeUserCount: 17,
-    imageUrl: "https://images.unsplash.com/photo-1598024055266-e772a5f8c128?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    imageUrl: "https://images.unsplash.com/photo-1562771379-eafdca7a02f8?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     challengeId: 10,
-    challengeTitle: "공부 시간을 늘리는 공부 챌린지",
+    challengeTitle: "외국어 학습",
     categoryName: "공부",
     challengeUserCount: 28,
-    imageUrl: "https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    imageUrl: "https://images.unsplash.com/photo-1460518451285-97b6aa326961?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   }
 
 ];
 
-exports.data = dummyChallengeAll;
+module.exports = dummyChallengeAll;
